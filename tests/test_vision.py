b18b6b74e93ec52ce424b5875e11b144a667d51f
asteloleh-gif <astel.oleh@gmail.com>
2026-09-17 11:37:08 -0500

    Fix timestamp helper expectations

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -4,8 +4,8 @@ from video_editor.vision import _timestamps
 def test_timestamps_cover_start_and_end():
     points = _timestamps(3.2, 1.0)
     assert points[0] == 0.0
-    assert points[-1] > 3.0
-    assert points[-1] < 3.2
+    assert points[-1] == 3.0
+    assert all(a < b for a, b in zip(points, points[1:]))
 
 
 def test_timestamps_short_clip():
